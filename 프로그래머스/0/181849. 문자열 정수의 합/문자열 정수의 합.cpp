e8c7c2dbf404dedfffa40f0e33a3abfe9e047502
k1ju <gksrlwn98@naver.com>
2024-04-20 22:27:30 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string num_str) {
+    
+    int answer = 0;
+    
+    for(char ch: num_str){
+        
+        answer+= ch - '0';        
+    }
+    return answer;
+}
