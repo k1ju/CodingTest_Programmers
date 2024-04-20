6343a9921aee594b2113f85f5e328907759d8635
k1ju <gksrlwn98@naver.com>
2024-04-21 00:41:41 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 vector<int> solution(vector<int> num_list) {
-    vector<int> answer = {0,0};
+    vector<int> answer(2,0);
     
     for(int i : num_list){
         if(i%2 == 0) {
