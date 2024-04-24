3b143a1ed90752738c9619d1799647aa2ce4c86e
k1ju <gksrlwn98@naver.com>
2024-04-25 00:08:06 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -1,21 +1,20 @@
 #include <string>
 #include <vector>
-#include <sstream>
 #include <iostream>
-
+#include <sstream>
 using namespace std;
 
 vector<string> solution(string my_string) {
     vector<string> answer;
     stringstream ss(my_string);
+    
     string word;
     
-    while(getline(ss, word,' ')){
-        if(word != ""){
-            answer.push_back(word);
-            cout << word << endl;
-            
-        }
+    
+    while(ss >> word){
+        
+        answer.push_back(word);
+        cout << word << endl;
     }
     
     
