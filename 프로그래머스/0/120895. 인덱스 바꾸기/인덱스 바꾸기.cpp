17e2261521bcc7c7306128366d0ae304857b066a
k1ju <gksrlwn98@naver.com>
2024-04-27 13:31:18 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //swap사용
 string solution(string my_string, int num1, int num2) {
         
-    swap(my_string[num1], my_string[num2] );
+   swap(my_string[num1], my_string[num2]);
     
     return my_string;
 }
