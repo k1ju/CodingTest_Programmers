69bcfee84932610d23082985ea9dae9b141f38c9
k1ju <gksrlwn98@naver.com>
2024-06-08 16:18:35 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,18 @@
+def solution(rsp):
+    
+    
+    answer = ''
+    
+    
+    
+    for x in rsp:
+        if x == '0':
+            answer+= '5'
+        elif x == '2':
+            answer += '0'
+        elif x == '5':
+            answer += '2'
+    
+    
+    
+    return answer
