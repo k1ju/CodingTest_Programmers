9efd497840d5fe01f9316f565b2e69bd5c4f3b99
k1ju <gksrlwn98@naver.com>
2024-09-20 23:35:16 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.50 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,25 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
         int[] answer = new int[score.length];
-        int[] honor = new int[k];
-        Arrays.fill(honor, Integer.MAX_VALUE);
         
         for(int i=0; i<score.length; i++){
-            if(i<k){
-                honor[i] = score[i];
-            } else{
-                int min = honor[0];
-                if(min < score[i]){
-                    honor[0] = score[i];
-                }
-            }
-            Arrays.sort(honor);
-            answer[i] = honor[0];
+            pq.add(score[i]);
+            
+            if(pq.size() > k){
+                pq.poll();  
+            } 
+            
+            answer[i] = pq.peek();
         }
-        
-        
-        
         
         return answer;
     }
