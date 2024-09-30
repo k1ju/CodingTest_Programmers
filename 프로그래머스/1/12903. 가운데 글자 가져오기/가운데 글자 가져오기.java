3a0c8d9d7a5591177f7976c818549be9464853b3
k1ju <gksrlwn98@naver.com>
2024-09-30 13:34:58 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.08 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+
+        
+        StringBuilder sb = new StringBuilder();
+        
+        if(s.length() % 2 == 0){
+            sb.append(s.substring(s.length()/2-1, s.length() / 2 + 1 ));   
+        } else{
+            sb.append(s.substring(s.length()/2, s.length() / 2 + 1)); 
+        }
+    
+        
+        return sb.toString();
+    }
+}
