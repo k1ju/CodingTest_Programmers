a57655a21e7c35ce188feac85570af6c5ce6133e
k1ju <gksrlwn98@naver.com>
2024-10-15 12:30:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.34 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,29 +2,30 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
+
+    String answer = "";
         
-        HashMap<String, Integer> hashMap = new HashMap<>();
+    HashMap<String, Integer> map = new HashMap<>();
         
-        for (int i=0; i<participant.length; i++) {
-            if(!hashMap.containsKey(participant[i])){
-                hashMap.put(participant[i], 1);
-            } else{
-                hashMap.put(participant[i], hashMap.get(participant[i]) +1 );
-            }
+    for(int i=0; i<participant.length; i++){
+        if(!map.containsKey(participant[i])){
+            map.put(participant[i], 1);
+        } else{
+            map.put(participant[i], map.get(participant[i]) +1 );
         }
+    }
+    
+    for(int i=0; i<completion.length; i++){
+        map.put(completion[i], map.get(completion[i]) -1 );
+    }
+    
+    for(int i=0; i<participant.length; i++){
         
-        for(int i=0; i<completion.length; i++){
-            hashMap.put(completion[i], hashMap.get(completion[i]) -1);
+        if(map.get(participant[i]) != 0){
+            answer = participant[i];
         }
-        
-        for (int i=0; i<participant.length; i++) {
-            if(hashMap.get(participant[i]) > 0){
-                answer = participant[i];
-                break;
-            }
-        }
-        
+    }
+    
         return answer;
     }
 }
