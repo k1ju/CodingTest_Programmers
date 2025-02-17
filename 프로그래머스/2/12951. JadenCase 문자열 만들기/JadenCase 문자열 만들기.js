e0852daa780211608eeab4667368661d0860a8c7
k1ju <gksrlwn98@naver.com>
2025-02-17 20:44:42 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.95 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -3,7 +3,7 @@ function solution(s) {
     
     for(let i=0; i<answer.length; i++){
         
-        answer[i] = answer[i].substring(0,1).toUpperCase() + answer[i].substring(1,answer[i].length).toLowerCase();
+        answer[i] = answer[i].substring(0,1).toUpperCase() + answer[i].substring(1).toLowerCase();
         
     }
     
